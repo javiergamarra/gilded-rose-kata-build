1db63e10fe0aeb90760be1a1a3b983c883a6fd00
Javier Gamarra <nhpatt@gmail.com>
2021-04-09 17:58:06 +0200

    Fix

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -7,10 +7,6 @@ class GildedRose {
         this.items = items;
     }
 
-    public Boolean getBoolean() {
-        return null;
-    }
-
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
             if (!items[i].name.equals("Aged Brie")
